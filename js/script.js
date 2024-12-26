23fb9ea0ab08b24788b05b3fbc409109c87b0761
Andrew Kononenko <wastardy.k@gmail.com>
2024-12-26 13:47:20 +0200

    add typing effect in header section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// Add custom JavaScript here
+function runTypingEffect() {
+    const text = 'I am Andrii Kononenko.';
+    const typingElement = document.getElementById('typing-text');
+    const typingDelay = 150;
+
+    typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, typingDelay) {
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(() => {
+            typingElement.textContent += text.charAt(i);
+        }, typingDelay * i);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', runTypingEffect);
